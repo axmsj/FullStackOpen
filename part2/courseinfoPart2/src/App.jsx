1fe6f6de63b731c12f254f96507467fbcbb9448b
Armando Gutierrez <115781553+axmsj@users.noreply.github.com.>
2026-05-23 22:20:41 -0400

    2.1 - 2.5 - Refactoring Course module

--- part2/courseinfoPart2/src/App.jsx
+++ part2/courseinfoPart2/src/App.jsx
@@ -1,37 +1,4 @@
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  console.log('CONTENT PROPS', props);
-  return props.name.map((x) => <Part key={x.id} name={x.name} exercises={x.exercises} />);
-};
-
-const Total = (props) => {
-  const initialValue = 0;
-  const exercises = props.parts.map((x) => x.exercises);
-  const sum = exercises.reduce((acc, curr) => acc + curr, initialValue);
-  return <b>total of {sum} exercises</b>;
-};
-
-const Course = ({ course }) => {
-  console.log('course', course);
-  return (
-    <>
-      <Header name={course.name} />
-      <Content name={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
